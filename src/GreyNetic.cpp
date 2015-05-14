cf579bfa750c0f77145228fe4ce3de1cdc79ec00
Anton Fedchin <afedchin@ruswizards.com>
2015-05-14 17:02:39 +0300

    [win32] Added support of DirectX 11 for Windows.

--- src/GreyNetic.cpp
+++ src/GreyNetic.cpp
@@ -35,17 +35,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#ifdef WIN32
+#include <d3d11.h>
+#endif
 
 #define MAX_BOXES 10000
 
 extern "C" {
 #include "GreyNetic.h"
-
-struct CUSTOMVERTEX
-{
-    float x, y, z, rhw; // The transformed position for the vertex
-    int color;        // The vertex color
-};
 
 	int		NumberOfBoxes = MAX_BOXES;
 
@@ -87,6 +84,95 @@ struct CUSTOMVERTEX
 	int ha[MAX_BOXES];
 	float dwcolor[MAX_BOXES][4];
 
+#ifdef WIN32
+  ID3D11DeviceContext* g_pContext = nullptr;
+  ID3D11Buffer*        g_pVBuffer = nullptr;
+  ID3D11PixelShader*   g_pPShader = nullptr;
+
+#define SAFE_RELEASE(_p)		{ if(_p) { _p->Release();	_p=NULL; } }
+
+const BYTE PixelShader[] =
+{
+     68,  88,  66,  67,  18, 124, 
+    182,  35,  30, 142, 196, 211, 
+     95, 130,  91, 204,  99,  13, 
+    249,   8,   1,   0,   0,   0, 
+    124,   1,   0,   0,   4,   0, 
+      0,   0,  48,   0,   0,   0, 
+    124,   0,   0,   0, 188,   0, 
+      0,   0,  72,   1,   0,   0, 
+     65, 111, 110,  57,  68,   0, 
+      0,   0,  68,   0,   0,   0, 
+      0,   2, 255, 255,  32,   0, 
+      0,   0,  36,   0,   0,   0, 
+      0,   0,  36,   0,   0,   0, 
+     36,   0,   0,   0,  36,   0, 
+      0,   0,  36,   0,   0,   0, 
+     36,   0,   0,   2, 255, 255, 
+     31,   0,   0,   2,   0,   0, 
+      0, 128,   0,   0,  15, 176, 
+      1,   0,   0,   2,   0,   8, 
+     15, 128,   0,   0, 228, 176, 
+    255, 255,   0,   0,  83,  72, 
+     68,  82,  56,   0,   0,   0, 
+     64,   0,   0,   0,  14,   0, 
+      0,   0,  98,  16,   0,   3, 
+    242,  16,  16,   0,   1,   0, 
+      0,   0, 101,   0,   0,   3, 
+    242,  32,  16,   0,   0,   0, 
+      0,   0,  54,   0,   0,   5, 
+    242,  32,  16,   0,   0,   0, 
+      0,   0,  70,  30,  16,   0, 
+      1,   0,   0,   0,  62,   0, 
+      0,   1,  73,  83,  71,  78, 
+    132,   0,   0,   0,   4,   0, 
+      0,   0,   8,   0,   0,   0, 
+    104,   0,   0,   0,   0,   0, 
+      0,   0,   1,   0,   0,   0, 
+      3,   0,   0,   0,   0,   0, 
+      0,   0,  15,   0,   0,   0, 
+    116,   0,   0,   0,   0,   0, 
+      0,   0,   0,   0,   0,   0, 
+      3,   0,   0,   0,   1,   0, 
+      0,   0,  15,  15,   0,   0, 
+    122,   0,   0,   0,   0,   0, 
+      0,   0,   0,   0,   0,   0, 
+      3,   0,   0,   0,   2,   0, 
+      0,   0,   3,   0,   0,   0, 
+    122,   0,   0,   0,   1,   0, 
+      0,   0,   0,   0,   0,   0, 
+      3,   0,   0,   0,   2,   0, 
+      0,   0,  12,   0,   0,   0, 
+     83,  86,  95,  80,  79,  83, 
+     73,  84,  73,  79,  78,   0, 
+     67,  79,  76,  79,  82,   0, 
+     84,  69,  88,  67,  79,  79, 
+     82,  68,   0, 171,  79,  83, 
+     71,  78,  44,   0,   0,   0, 
+      1,   0,   0,   0,   8,   0, 
+      0,   0,  32,   0,   0,   0, 
+      0,   0,   0,   0,   0,   0, 
+      0,   0,   3,   0,   0,   0, 
+      0,   0,   0,   0,  15,   0, 
+      0,   0,  83,  86,  95,  84, 
+     65,  82,  71,  69,  84,   0, 
+    171, 171
+};
+
+void InitDXStuff(void)
+{
+  ID3D11Device* pDevice = nullptr;
+  g_pContext->GetDevice(&pDevice);
+
+  CD3D11_BUFFER_DESC vbDesc(sizeof(MYCUSTOMVERTEX) * 5, D3D11_BIND_VERTEX_BUFFER, D3D11_USAGE_DYNAMIC, D3D11_CPU_ACCESS_WRITE);
+  pDevice->CreateBuffer(&vbDesc, nullptr, &g_pVBuffer);
+
+  pDevice->CreatePixelShader(PixelShader, sizeof(PixelShader), nullptr, &g_pPShader);
+
+  SAFE_RELEASE(pDevice);
+}
+#endif
+
 ////////////////////////////////////////////////////////////////////////////
 // XBMC has loaded us into memory, we should set our core values
 // here and load any settings we may have from our config file
@@ -100,7 +186,10 @@ ADDON_STATUS ADDON_Create(void* hdl, void* props)
 
   m_iWidth = scrprops->width;
   m_iHeight = scrprops->height;
-
+#ifdef WIN32
+  g_pContext = reinterpret_cast<ID3D11DeviceContext*>(scrprops->device);
+  InitDXStuff();
+#endif
   return ADDON_STATUS_NEED_SETTINGS;
 }
 
@@ -119,23 +208,32 @@ extern "C" void Start()
 
 void DrawRectangle(int x, int y, int w, int h, float* dwColour)
 {
-	void* pVertices;
-    //MYCUSTOMVERTEX cvVertices[300*4];
     //Store each point of the triangle together with it's colour
     MYCUSTOMVERTEX cvVertices[] =
     {
-        {(float) x, (float) y+h, 0.0f, 0.5, dwColour[0], dwColour[1], dwColour[2], dwColour[3]},
-        {(float) x, (float) y, 0.0f, 0.5, dwColour[0], dwColour[1], dwColour[2], dwColour[3]},
-		{(float) x+w, (float) y+h, 0.0f, 0.5, dwColour[0], dwColour[1], dwColour[2], dwColour[3]},
-        {(float) x+w, (float) y, 0.0f, 0.5,  dwColour[0], dwColour[1], dwColour[2], dwColour[3]},
+        {(float)   x, (float) y+h, 0.0f, dwColour[0], dwColour[1], dwColour[2], dwColour[3]},
+        {(float)   x, (float)   y, 0.0f, dwColour[0], dwColour[1], dwColour[2], dwColour[3]},
+        {(float) x+w, (float) y+h, 0.0f, dwColour[0], dwColour[1], dwColour[2], dwColour[3]},
+        {(float) x+w, (float)   y, 0.0f, dwColour[0], dwColour[1], dwColour[2], dwColour[3]},
     };
+#ifndef WIN32
     glBegin(GL_TRIANGLE_STRIP);
     for (size_t j=0;j<4;++j)
     {
-      glColor4f(cvVertices[j].r/255.0, cvVertices[j].g/255.0, cvVertices[j].b/255.0, cvVertices[j].a/255.0);
+      glColor4f(cvVertices[j].r, cvVertices[j].g, cvVertices[j].b, cvVertices[j].a);
       glVertex2f(cvVertices[j].x, cvVertices[j].y);
     }
     glEnd();
+#else
+    D3D11_MAPPED_SUBRESOURCE res = {};
+    if (SUCCEEDED(g_pContext->Map(g_pVBuffer, 0, D3D11_MAP_WRITE_DISCARD, 0, &res)))
+    {
+      memcpy(res.pData, cvVertices, sizeof(cvVertices));
+      g_pContext->Unmap(g_pVBuffer, 0);
+    }
+    g_pContext->Draw(4, 0);
+
+#endif
 }
 
 // XBMC tells us to render a frame of
@@ -145,8 +243,12 @@ void DrawRectangle(int x, int y, int w, int h, float* dwColour)
 // device will already have been cleared.
 extern "C" void Render()
 {
-
-
+#ifdef WIN32
+  g_pContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP);
+  size_t strides = sizeof(MYCUSTOMVERTEX), offsets = 0;
+  g_pContext->IASetVertexBuffers(0, 1, &g_pVBuffer, &strides, &offsets);
+  g_pContext->PSSetShader(g_pPShader, NULL, 0);
+#endif
 
 	for (int i=NumberOfBoxes - 1 ; i>0; i--){
 		xa[i] = xa[i-1] ;
@@ -181,10 +283,10 @@ extern "C" void Render()
 
 
 
-	dwcolor[0][0] = red;
-        dwcolor[0][1] = green;
-        dwcolor[0][2] = blue;
-        dwcolor[0][3] = alpha;
+	dwcolor[0][0] = (float)red   / 255.0f;
+	dwcolor[0][1] = (float)green / 255.0f;
+	dwcolor[0][2] = (float)blue  / 255.0f;
+	dwcolor[0][3] = (float)alpha / 255.0f;
 	
 	xa[0] = rand()%m_iWidth;	
 	ya[0] = rand()%m_iHeight;	
@@ -218,7 +320,11 @@ extern "C" void Render()
 // any resources we have created.
 extern "C" void ADDON_Stop()
 {
-	return;
+#ifdef WIN32
+  SAFE_RELEASE(g_pPShader);
+  SAFE_RELEASE(g_pVBuffer);
+#endif
+  return;
 }
 
 void ADDON_Destroy()
