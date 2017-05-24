d33fabab1b87e9a691e5eff9c3522b8d261b836a
Alwin Esch <alwin.esch@web.de>
2017-05-24 06:20:13 +0200

    Change to new C++ based addon interface style

--- src/GreyNetic.cpp
+++ src/GreyNetic.cpp
@@ -1,6 +1,6 @@
 /*
  * GreyNetic Screensaver for XBox Media Center
- * Copyright (c) 2004 Team XBMC
+ * Copyright (c) 2004 Team Kodi
  *
  * Ver 1.0 26 Fed 2005	Dylan Thurston (Dinomight)
  *
@@ -29,7 +29,7 @@
 */
 
 
-#include "xbmc_scr_dll.h"
+#include <kodi/addon-instance/Screensaver.h>
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -42,55 +42,58 @@
 
 #define MAX_BOXES 10000
 
-extern "C" {
-#include "GreyNetic.h"
+int NumberOfBoxes = MAX_BOXES;
 
-	int		NumberOfBoxes = MAX_BOXES;
-
-	int		MaxSizeX = 200;
-	int		MinSizeX = 0;
-	int		MaxSizeY = 200;
-	int		MinSizeY = 0;
-	int		MaxSquareSize = 200;
-	int		MinSquareSize = 0;
-	int		MaxAlpha = 255;
-	int		MinAlpha = 0;
-	int		MaxRed = 255;
-	int		MinRed = 0;
-	int		MaxGreen = 255;
-	int		MinGreen = 0;
-	int		MaxBlue = 255;
-	int		MinBlue = 0;
-	int		MaxJoined = 255;
-	int		MinJoined = 0;
+int MaxSizeX = 200;
+int MinSizeX = 0;
+int MaxSizeY = 200;
+int MinSizeY = 0;
+int MaxSquareSize = 200;
+int MinSquareSize = 0;
+int MaxAlpha = 255;
+int MinAlpha = 0;
+int MaxRed = 255;
+int MinRed = 0;
+int MaxGreen = 255;
+int MinGreen = 0;
+int MaxBlue = 255;
+int MinBlue = 0;
+int MaxJoined = 255;
+int MinJoined = 0;
 
 
-	bool	MakeSquares = false;
-	bool	JoinedSizeX = false;
-	bool	JoinedSizeY = false;
-	bool	JoinedRed = false;
-	bool	JoinedGreen = false;
-	bool	JoinedBlue = false;
-	bool	JoinedAlpha = false;
+bool MakeSquares = false;
+bool JoinedSizeX = false;
+bool JoinedSizeY = false;
+bool JoinedRed = false;
+bool JoinedGreen = false;
+bool JoinedBlue = false;
+bool JoinedAlpha = false;
 
 
-	int		m_width;
-	int		m_height;
-	float		m_centerx;
-	float		m_centery;
-	
-	int xa[MAX_BOXES];
-	int ya[MAX_BOXES];
-	int wa[MAX_BOXES];
-	int ha[MAX_BOXES];
-	float dwcolor[MAX_BOXES][4];
+int m_width;
+int m_height;
+float m_centerx;
+float m_centery;
+
+int xa[MAX_BOXES];
+int ya[MAX_BOXES];
+int wa[MAX_BOXES];
+int ha[MAX_BOXES];
+float dwcolor[MAX_BOXES][4];
+
+struct MYCUSTOMVERTEX
+{
+  float x, y, z; // The transformed position for the vertex.
+  float r, g, b, a;
+};
 
 #ifdef WIN32
   ID3D11DeviceContext* g_pContext = nullptr;
   ID3D11Buffer*        g_pVBuffer = nullptr;
   ID3D11PixelShader*   g_pPShader = nullptr;
 
-#define SAFE_RELEASE(_p)		{ if(_p) { _p->Release();	_p=NULL; } }
+#define SAFE_RELEASE(_p) { if(_p) { _p->Release();	_p=NULL; } }
 
 const BYTE PixelShader[] =
 {
@@ -167,47 +170,153 @@ void InitDXStuff(void)
 
   CD3D11_BUFFER_DESC vbDesc(sizeof(MYCUSTOMVERTEX) * 5, D3D11_BIND_VERTEX_BUFFER, D3D11_USAGE_DYNAMIC, D3D11_CPU_ACCESS_WRITE);
   pDevice->CreateBuffer(&vbDesc, nullptr, &g_pVBuffer);
-
   pDevice->CreatePixelShader(PixelShader, sizeof(PixelShader), nullptr, &g_pPShader);
 
   SAFE_RELEASE(pDevice);
 }
 #endif
 
+class CScreensaverGreyNetic
+  : public kodi::addon::CAddonBase,
+    public kodi::addon::CInstanceScreensaver
+{
+public:
+  CScreensaverGreyNetic();
+  virtual ~CScreensaverGreyNetic();
+
+  virtual void Render() override;
+
+private:
+  int m_iWidth;
+  int m_iHeight;
+
+  void DrawRectangle(int x, int y, int w, int h, float* dwColour);
+};
+
 ////////////////////////////////////////////////////////////////////////////
-// XBMC has loaded us into memory, we should set our core values
+// Kodi has loaded us into memory, we should set our core values
 // here and load any settings we may have from our config file
 //
-ADDON_STATUS ADDON_Create(void* hdl, void* props)
+CScreensaverGreyNetic::CScreensaverGreyNetic()
 {
-  if (!props)
-    return ADDON_STATUS_UNKNOWN;
+  m_iWidth = Width();
+  m_iHeight = Height();
 
-  AddonProps_Screensaver* scrprops = (AddonProps_Screensaver*)props;
+  NumberOfBoxes = kodi::GetSettingInt("boxes");
+  MakeSquares = kodi::GetSettingBoolean("square");
+  MaxSizeX = kodi::GetSettingInt("maxsizex");
+  MaxSizeY = kodi::GetSettingInt("maxsizey");
+  MinSizeX = kodi::GetSettingInt("minsizex");
+  MinSizeY = kodi::GetSettingInt("minsizey");
+  MinSquareSize = kodi::GetSettingInt("minsize");
+  MaxSquareSize = kodi::GetSettingInt("maxsize");
+  MinAlpha = kodi::GetSettingInt("minalpha");
+  MinRed = kodi::GetSettingInt("minred");
+  MaxRed = kodi::GetSettingInt("maxred");
+  MinGreen = kodi::GetSettingInt("mingreen");
+  MaxGreen = kodi::GetSettingInt("maxgreen");
+  MinBlue = kodi::GetSettingInt("minblue");
+  MaxBlue = kodi::GetSettingInt("maxblue");
 
-  m_iWidth = scrprops->width;
-  m_iHeight = scrprops->height;
 #ifdef WIN32
   g_pContext = reinterpret_cast<ID3D11DeviceContext*>(scrprops->device);
   InitDXStuff();
 #endif
-  return ADDON_STATUS_NEED_SETTINGS;
+
 }
 
-
-
-
-// XBMC tells us we should get ready
-// to start rendering. This function
-// is called once when the screensaver
-// is activated by XBMC.
-extern "C" void Start()
+// Kodi tells us to stop the screensaver
+// we should free any memory and release
+// any resources we have created.
+CScreensaverGreyNetic::~CScreensaverGreyNetic()
 {
-	return;
+#ifdef WIN32
+  SAFE_RELEASE(g_pPShader);
+  SAFE_RELEASE(g_pVBuffer);
+#endif
 }
 
+// Kodi tells us to render a frame of
+// our screensaver. This is called on
+// each frame render in Kodi, you should
+// render a single frame only - the DX
+// device will already have been cleared.
+void CScreensaverGreyNetic::Render()
+{
+#ifdef WIN32
+  g_pContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP);
+  UINT strides = sizeof(MYCUSTOMVERTEX), offsets = 0;
+  g_pContext->IASetVertexBuffers(0, 1, &g_pVBuffer, &strides, &offsets);
+  g_pContext->PSSetShader(g_pPShader, NULL, 0);
+#endif
 
-void DrawRectangle(int x, int y, int w, int h, float* dwColour)
+  for (int i=NumberOfBoxes - 1 ; i>0; i--)
+  {
+    xa[i] = xa[i-1] ;
+    ya[i] = ya[i-1] ;
+    ha[i] = ha[i-1] ;
+    wa[i] = wa[i-1] ;
+    dwcolor[i][0] = dwcolor[i-1][0] ;
+    dwcolor[i][1] = dwcolor[i-1][1] ;
+    dwcolor[i][2] = dwcolor[i-1][2] ;
+    dwcolor[i][3] = dwcolor[i-1][3] ;
+  }
+
+  double red = rand() %(MaxRed - MinRed) + MinRed;
+  double green = rand() %(MaxGreen - MinGreen) + MinGreen;
+  double blue = rand() %(MaxBlue - MinBlue) + MinBlue;
+  double alpha = rand() %(MaxAlpha - MinAlpha) + MinAlpha;
+  double joined = rand() %(MaxJoined - MinJoined) + MinJoined;
+ 
+  if(JoinedRed){
+    red = joined;
+  }
+  if(JoinedGreen){
+    green = joined;
+  }
+  if(JoinedBlue){
+    blue = joined;
+  }
+  if(JoinedAlpha){
+    alpha = joined;
+  }
+
+  dwcolor[0][0] = (float)red   / 255.0f;
+  dwcolor[0][1] = (float)green / 255.0f;
+  dwcolor[0][2] = (float)blue  / 255.0f;
+  dwcolor[0][3] = (float)alpha / 255.0f;
+  
+  xa[0] = rand()%m_iWidth;
+  ya[0] = rand()%m_iHeight;
+
+  ha[0] = rand() % (MaxSizeY - MinSizeY) + MinSizeY;
+  wa[0] = rand() % (MaxSizeX - MinSizeX) + MinSizeX;
+
+  if(MakeSquares)
+  {
+    ha[0] = rand() % (MaxSquareSize - MinSquareSize) + MinSquareSize;
+    wa[0] = ha[0];
+  }
+
+  if(JoinedSizeY)
+  {
+    ha[0] = joined;
+  }
+  if(JoinedSizeX)
+  {
+    wa[0] = joined;
+  }
+
+  //ha[0] = wa[0];
+
+  for (int i=NumberOfBoxes - 1 ; i>0; i--)
+  {
+    DrawRectangle(xa[i],ya[i],wa[i],ha[i], dwcolor[i]);
+  }
+  return;
+}
+
+void CScreensaverGreyNetic::DrawRectangle(int x, int y, int w, int h, float* dwColour)
 {
     //Store each point of the triangle together with it's colour
     MYCUSTOMVERTEX cvVertices[] =
@@ -237,141 +346,4 @@ void DrawRectangle(int x, int y, int w, int h, float* dwColour)
 #endif
 }
 
-// XBMC tells us to render a frame of
-// our screensaver. This is called on
-// each frame render in XBMC, you should
-// render a single frame only - the DX
-// device will already have been cleared.
-extern "C" void Render()
-{
-#ifdef WIN32
-  g_pContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP);
-  UINT strides = sizeof(MYCUSTOMVERTEX), offsets = 0;
-  g_pContext->IASetVertexBuffers(0, 1, &g_pVBuffer, &strides, &offsets);
-  g_pContext->PSSetShader(g_pPShader, NULL, 0);
-#endif
-
-	for (int i=NumberOfBoxes - 1 ; i>0; i--){
-		xa[i] = xa[i-1] ;
-		ya[i] = ya[i-1] ;
-		ha[i] = ha[i-1] ;
-		wa[i] = wa[i-1] ;
-		dwcolor[i][0] = dwcolor[i-1][0] ;
-		dwcolor[i][1] = dwcolor[i-1][1] ;
-		dwcolor[i][2] = dwcolor[i-1][2] ;
-		dwcolor[i][3] = dwcolor[i-1][3] ;
-
-	}
-
-		double red = rand() %(MaxRed - MinRed) + MinRed;
-		double green = rand() %(MaxGreen - MinGreen) + MinGreen;
-		double blue = rand() %(MaxBlue - MinBlue) + MinBlue;
-		double alpha = rand() %(MaxAlpha - MinAlpha) + MinAlpha;
-		double joined = rand() %(MaxJoined - MinJoined) + MinJoined;
-		
-		if(JoinedRed){
-			red = joined;
-		}
-		if(JoinedGreen){
-			green = joined;
-		}
-		if(JoinedBlue){
-			blue = joined;
-		}
-		if(JoinedAlpha){
-			alpha = joined;
-		}
-
-
-
-	dwcolor[0][0] = (float)red   / 255.0f;
-	dwcolor[0][1] = (float)green / 255.0f;
-	dwcolor[0][2] = (float)blue  / 255.0f;
-	dwcolor[0][3] = (float)alpha / 255.0f;
-	
-	xa[0] = rand()%m_iWidth;	
-	ya[0] = rand()%m_iHeight;	
-	
-	ha[0] = rand() % (MaxSizeY - MinSizeY) + MinSizeY;	
-	wa[0] = rand() % (MaxSizeX - MinSizeX) + MinSizeX;
-	
-
-	if(MakeSquares){
-		ha[0] = rand() % (MaxSquareSize - MinSquareSize) + MinSquareSize;	
-		wa[0] = ha[0]; 
-	}
-
-	if(JoinedSizeY){
-		ha[0] = joined;
-	}
-	if(JoinedSizeX){
-		wa[0] = joined;
-	}
-
-	//ha[0] = wa[0];
-
-	for (int i=NumberOfBoxes - 1 ; i>0; i--){	
-		DrawRectangle(xa[i],ya[i],wa[i],ha[i], dwcolor[i]);
-	}
-	return;
-}
-
-// XBMC tells us to stop the screensaver
-// we should free any memory and release
-// any resources we have created.
-extern "C" void Stop()
-{
-#ifdef WIN32
-  SAFE_RELEASE(g_pPShader);
-  SAFE_RELEASE(g_pVBuffer);
-#endif
-  return;
-}
-
-void ADDON_Destroy()
-{
-}
-
-ADDON_STATUS ADDON_GetStatus()
-{
-  return ADDON_STATUS_OK;
-}
-
-ADDON_STATUS ADDON_SetSetting(const char *strSetting, const void *value)
-{
-  if (strcmp(strSetting,"boxes") == 0)
-   NumberOfBoxes = *(int*)value;
-  if (strcmp(strSetting,"square") == 0)
-    MakeSquares = *(bool*)value;
-  if (strcmp(strSetting,"maxsizex") == 0)
-    MaxSizeX = *(int*)value;
-  if (strcmp(strSetting,"maxsizey") == 0)
-    MaxSizeY = *(int*)value;
-  if (strcmp(strSetting,"minsizex") == 0)
-    MinSizeX = *(int*)value;
-  if (strcmp(strSetting,"minsizey") == 0)
-    MinSizeY = *(int*)value;
-  if (strcmp(strSetting,"minsize") == 0)
-    MinSquareSize = *(int*)value;
-  if (strcmp(strSetting,"maxsize") == 0)
-    MaxSquareSize = *(int*)value;
-  if (strcmp(strSetting,"minalpha") == 0)
-    MinAlpha = *(int*)value;
-  if (strcmp(strSetting,"maxalpha") == 0)
-    MaxAlpha = *(int*)value;
-  if (strcmp(strSetting,"minred") == 0)
-    MinRed = *(int*)value;
-  if (strcmp(strSetting,"maxred") == 0)
-    MaxRed = *(int*)value;
-  if (strcmp(strSetting,"mingreen") == 0)
-    MinGreen = *(int*)value;
-  if (strcmp(strSetting,"maxgreen") == 0)
-    MaxGreen = *(int*)value;
-  if (strcmp(strSetting,"minblue") == 0)
-    MinBlue = *(int*)value;
-  if (strcmp(strSetting,"maxblue") == 0)
-    MaxBlue = *(int*)value;
-
-  return ADDON_STATUS_OK;
-}
-};
+ADDONCREATOR(CScreensaverGreyNetic);
